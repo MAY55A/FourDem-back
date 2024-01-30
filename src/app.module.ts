8334bf9fe66bb13a42489c3ff4a9c9a6e48d652c
mayssaGH <mayssagnm@gmail.com>
2024-01-30 11:14:28 +0100

    creation et configuration de la connexion avec BD

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserService } from './services/user.service';
+import { UsersController } from './controllers/users.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'fourdem',
+      username: 'root',
+      password: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
